9f6222dcb1ab01832f89da68823677cc9781f930
Malik <malikbilalge2000@gmail.com>
2023-07-12 20:12:17 +0500

    madde

--- src/controllers/noteController.js
+++ src/controllers/noteController.js
@@ -20,46 +20,19 @@ const createNote = async (req, res) => {
     }
 }
 
-const updateNote = async (req, res) => {
-    const id = req.params.id;
-
-    const { reqTitle, reqDescription } = req.body;
-    const newNote = ({
-        title: reqTitle,
-        description: reqDescription,
-        userId: req.userId
-    })
-
-    try {
-        await noteModel.findByIdAndUpdate(id, newNote, { new: true });
-        res.status(200).json(newNote);
-    }
-    catch (error) {
-        console.log(error);
-        res.status(500).json({ message: "Something went wrong" });
-    }
-
+const updateNote = (req, res) => {
+    console.log(req.userId)
+    res.send(req.userId);
 }
 
-const deleteNote = async (req, res) => {
-
-    const id = req.params.id;
-
-    try {
-        const note = await noteModel.findByIdAndRemove(id);
-        res.status(202).json(note);
-    }
-    catch (error) {
-        console.log(error);
-        res.status(500).json({ message: "Something went wrong" });
-    }
+const deleteNote = (req, res) => {
     console.log(req.userId)
     res.send(req.userId);
 }
 
 const getNote = async (req, res) => {
     try {
-        const notes = await noteModel.find({ userId: req.userId });
+        const notes = await noteModel.find({userId: req.userId});
         res.status(200).json(notes);
     }
     catch (error) {
